c442f4bf958591d026f6d21c80ecec6daafe62ae
fz0000 <fz0000@sina.com>
2024-07-01 16:46:57 +0800

    添加项目文件。

---
+++ otp/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace otp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonRefresh = new System.Windows.Forms.Button();
+            this.textBoxSecret = new System.Windows.Forms.TextBox();
+            this.textBoxCode = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonRefresh
+            // 
+            this.buttonRefresh.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonRefresh.Location = new System.Drawing.Point(102, 52);
+            this.buttonRefresh.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonRefresh.Name = "buttonRefresh";
+            this.buttonRefresh.Size = new System.Drawing.Size(92, 35);
+            this.buttonRefresh.TabIndex = 0;
+            this.buttonRefresh.Text = "Refresh";
+            this.buttonRefresh.Click += new System.EventHandler(this.buttonRefresh_Click);
+            // 
+            // textBoxSecret
+            // 
+            this.textBoxSecret.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.textBoxSecret.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxSecret.Location = new System.Drawing.Point(102, 14);
+            this.textBoxSecret.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBoxSecret.Name = "textBoxSecret";
+            this.textBoxSecret.Size = new System.Drawing.Size(200, 21);
+            this.textBoxSecret.TabIndex = 1;
+            this.textBoxSecret.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBoxCode
+            // 
+            this.textBoxCode.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.textBoxCode.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxCode.Location = new System.Drawing.Point(102, 114);
+            this.textBoxCode.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBoxCode.Name = "textBoxCode";
+            this.textBoxCode.ReadOnly = true;
+            this.textBoxCode.Size = new System.Drawing.Size(200, 21);
+            this.textBoxCode.TabIndex = 2;
+            this.textBoxCode.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.textBoxSecret, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBoxCode, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.buttonRefresh, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(354, 230);
+            this.tableLayoutPanel1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Secret Key:";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(62, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Code:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(354, 230);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "TOTP Sample App";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonRefresh;
+        private System.Windows.Forms.TextBox textBoxSecret;
+        private System.Windows.Forms.TextBox textBoxCode;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ otp/Form1.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Win32;
+using OtpNet;
+using System;
+using System.Windows.Forms;
+
+namespace otp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBoxSecret.Text = GetSecretKey();
+        }
+
+        private void buttonRefresh_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string strSecretKey = textBoxSecret.Text;
+                byte[] secretKey = Base32Encoding.ToBytes(strSecretKey);
+                Totp totp = new Totp(secretKey);
+                string totp_now = totp.ComputeTotp();
+                textBoxCode.Text = totp_now;
+                SaveSecretKey(strSecretKey);
+            }
+            catch
+            {
+                textBoxCode.Text = "Invalid SecretKey";
+            }
+        }
+        private void SaveSecretKey(string strSecretKey)
+        {
+            try
+            {
+                RegistryKey rk = Registry.CurrentUser.CreateSubKey(@"Software\TotpSecret");
+                rk.SetValue("key", strSecretKey);
+            }
+            catch
+            {
+                return;
+            }
+        }
+
+        private string GetSecretKey()
+        {
+            try
+            {
+                RegistryKey rk_app = Registry.CurrentUser.OpenSubKey(@"Software\TotpSecret");
+                return rk_app.GetValue("key").ToString();
+            }
+            catch
+            {
+                return "";
+            }
+        }
+    }
+}
